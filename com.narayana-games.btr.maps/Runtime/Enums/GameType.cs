2016e9a1a03d601765204797bb85a6da028845de
Jashan Chittesh <jashan@narayana-games.net>
2020-01-27 22:49:55 +0100

    Extended for BSMG map support

--- com.narayana-games.btr.maps/Runtime/Enums/GameType.cs
+++ com.narayana-games.btr.maps/Runtime/Enums/GameType.cs
@@ -7,7 +7,8 @@ namespace NarayanaGames.BeatTheRhythm.Maps.Enums {
         HolodanceCustom,
         osu,
         StepMania,
-        HolodanceProcedural
+        HolodanceProcedural,
+        BSMG
     }
 
 }
--- com.narayana-games.btr.maps/Runtime/Enums/PickupType.cs
+++ com.narayana-games.btr.maps/Runtime/Enums/PickupType.cs
@@ -2,9 +2,10 @@
 
     [System.Serializable]
     public enum PickupType {
-        Any = 0, // catch it however you like; also used when not caught
-        Left = 1, // hand, obviously ;-)
-        Right = 2, // hand, obviously ;-)
+        None = -1,    // AVOID, bombs or the like that players must not catch
+        Any = 0,      // catch it however you like; also used when not caught
+        Left = 1,     // hand, obviously ;-)
+        Right = 2,    // hand, obviously ;-)
         Head = 3,
         LeftFoot = 4, // when you have Vive trackers ...
         RightFoot = 5 // when you have Vive trackers ...
