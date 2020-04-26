09130c07de5a5de21fecff2af151680d765fee86
Jashan Chittesh <jashan@narayana-games.net>
2020-04-26 13:39:33 +0200

    Comment / semantic change
    Orbs that are originally intended to be caught with hands can always also be caught with feet (but not the other way round), so while originally Left/Right meant left or right hand, this now means left hand or foot, and right hand or foot

--- Package-Maps/com.narayana-games.btr.maps/Runtime/Enums/PickupType.cs
+++ Package-Maps/com.narayana-games.btr.maps/Runtime/Enums/PickupType.cs
@@ -4,8 +4,8 @@
     public enum PickupType {
         None = -1,    // AVOID, bombs or the like that players must not catch
         Any = 0,      // catch it however you like; also used when not caught
-        Left = 1,     // hand, obviously ;-)
-        Right = 2,    // hand, obviously ;-)
+        Left = 1,     // hand or foot
+        Right = 2,    // hand or foot
         Head = 3,
         LeftFoot = 4, // when you have Vive trackers ...
         RightFoot = 5 // when you have Vive trackers ...
